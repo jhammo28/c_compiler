1ff158a9f7e5631845aa211943f526adccd6553b
Jeff Hammond <jhammo28@vols.utk.edu>
2024-12-28 13:40:05 -0600

    assembly file generated

---
+++ return2.s
@@ -0,0 +1,10 @@
+	.file	"return2.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	movl	$2, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (GNU) 14.2.1 20240910"
+	.section	.note.GNU-stack,"",@progbits
